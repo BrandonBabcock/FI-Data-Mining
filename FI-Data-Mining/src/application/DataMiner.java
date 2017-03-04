21d2d82ea261b64ab54855a3a30d2b286ac278be
Brandon Babcock <brandonbabcock3@gmail.com>
2017-03-04 16:44:47 -0500

    Added Gradle Integration and DataMiner Class
    - Integrated Gradle with the current project to help manage dependencies
    - Added the DataMiner class that uses the Apriori association ruling
    algorithm to find rules within a data set

---
+++ FI-Data-Mining/src/application/DataMiner.java
@@ -0,0 +1,23 @@
+package application;
+
+import weka.associations.Apriori;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class DataMiner {
+
+	public void findSimilarities(String filePath) throws Exception {
+		// Load data
+		Instances data = DataSource.read(filePath);
+		data.setClassIndex(data.numAttributes() - 1);
+
+		// Build associator
+		Apriori apriori = new Apriori();
+		apriori.setClassIndex(data.classIndex());
+		apriori.buildAssociations(data);
+
+		// Output associator
+		System.out.println(apriori);
+	}
+
+}
