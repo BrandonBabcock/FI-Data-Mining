a5a5c3fa7dd79fbd135052af37a39589012b46a6
sreece52 <shawnjreece21@hotmail.com>
2017-03-05 14:58:53 -0500

    GUI work
    - split up scenes in main into different classes 
    - started the pre-process GUI
    - changed logic in event handlers

---
+++ FI-Data-Mining/src/application/GUI/PreProcessingScene.java
@@ -0,0 +1,64 @@
+package application.GUI;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+/**
+ * Created by shawn on 3/5/2017.
+ */
+public class PreProcessingScene {
+
+    Button previousButton = new Button("Previous");
+    Button nextButton = new Button("Next");
+
+    public Scene preProcessingScene(){
+        BorderPane borderPane = new BorderPane();
+        borderPane.setPadding(new Insets(20, 5, 5, 5));
+
+        // Create the header and add it to the border pane
+        Text headerText = new Text("Fischer International Data Mining");
+        Font headerFont = Font.font("Times New Roman", FontWeight.BOLD, 48);
+        headerText.setFont(headerFont);
+        borderPane.setTop(headerText);
+        BorderPane.setAlignment(headerText, Pos.TOP_CENTER);
+
+        // Create step info nodes
+        Font stepInfoFont = Font.font("Times New Roman", FontWeight.BOLD, 28);
+        Text currentStepText = new Text("Step 2/3:");
+        currentStepText.setFont(stepInfoFont);
+        Text stepDescriptionText = new Text("Pre-process");
+        stepDescriptionText.setFont(stepInfoFont);
+
+        VBox stepInfoBox = new VBox();
+        stepInfoBox.setAlignment(Pos.TOP_CENTER);
+        stepInfoBox.setPadding(new Insets(200, 0, 25, 0));
+        stepInfoBox.getChildren().addAll(currentStepText, stepDescriptionText);
+
+        // Add navigation buttons to a horizontal box
+        HBox navigationBox = new HBox(25);
+        navigationBox.setAlignment(Pos.TOP_CENTER);
+        navigationBox.setPadding(new Insets(100, 0, 0, 0));
+        navigationBox.getChildren().addAll(previousButton, nextButton);
+
+        // Add step info box, configuration grid pane, and navigation buttons
+        // box to a vertical box container
+        VBox centerContent = new VBox(20);
+        centerContent.getChildren().addAll(stepInfoBox,navigationBox);
+
+        // Sets the border pane's center as centerContent
+        borderPane.setCenter(centerContent);
+
+        // Configure scene
+        Scene scene = new Scene(borderPane, 1080, 720);
+        return scene;
+    }
+
+}
