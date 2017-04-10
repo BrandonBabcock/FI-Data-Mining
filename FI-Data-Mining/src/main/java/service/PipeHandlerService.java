d218e5ea330d18b1b0de5d74cab91e8989e9fd4c
sreece52 <shawnjreece21@hotmail.com>
2017-04-10 00:07:44 -0400

    Pipe Handler updates

--- FI-Data-Mining/src/main/java/service/PipeHandlerService.java
+++ FI-Data-Mining/src/main/java/service/PipeHandlerService.java
@@ -14,11 +14,16 @@ public class PipeHandlerService {
     private String[] originalFileAttribute;
     private LinkedHashSet<Integer> hasPipes;
     private HashSet<String> pipedAttributes;
-    private ArrayList<Object> newAttributes;
+    private ArrayList<String> newAttributes;
+    private ArrayList<String[]> data;
+    private ArrayList<ArrayList<String>> newData;
 
     public PipeHandlerService(File file){
         this.file = file;
+        newAttributes = new ArrayList<>();
         pipedAttributes = new HashSet<>();
+        data = new ArrayList<>();
+        newData = new ArrayList<>();
     }
 
     public static void main(String[] args){
@@ -43,6 +48,7 @@ public class PipeHandlerService {
         String[] values;
         while(scan.hasNext()){
             values = scan.nextLine().split(",");
+            data.add(values);
             for(int i = 0; i < values.length; i++){
                 if(values[i].contains("|")){
                     hasPipes.add(i);
@@ -55,59 +61,101 @@ public class PipeHandlerService {
     }
 
     public void buildAttributes() throws FileNotFoundException {
-        Scanner scan = new Scanner(file);
-        scan.nextLine();
-        String[] values;
-        while(scan.hasNext()){
-            values = scan.nextLine().split(",");
+        for(String[] str : data){
             for(Integer i : hasPipes){
-                String[] temp = values[i].split("\\|");
+                String[] temp = str[i].split("\\|");
                 for(String s : temp) {
                     pipedAttributes.add(originalFileAttribute[i] + "." + s.split("-")[0].replace(" ","_"));
                 }
             }
         }
-        scan.close();
+
+        updateData();
+    }
+
+    public String[] splitValues(String str){
+        return str.split("\\|");
+    }
+
+
+    public void updateData(){
+
+        for(int i = 0; i < originalFileAttribute.length; i++){
+            if(!hasPipes.contains(i)){
+                newAttributes.add(originalFileAttribute[i]);
+            }
+        }
+
+        for(String s : pipedAttributes){
+            newAttributes.add(s);
+        }
+
+        for(String[] row : data){
+            ArrayList<String> values = new ArrayList();
+            for(int i = 0; i < row.length; i++){
+                if(!hasPipes.contains(i)){
+                    values.add(row[i]);
+                }
+            }
+
+            for(int i = 0; i < row.length; i++){
+                if(hasPipes.contains(i)){
+                    if(row[i].contains("|")){
+                        String[] splitValues = splitValues(row[i]);
+                        values.addAll(Arrays.asList(splitValues));
+                    }
+                }
+            }
+            newData.add(values);
+        }
+
     }
 
     public void updateFile() throws FileNotFoundException {
         StringBuilder sb = new StringBuilder();
 
-        // remove the piped attributes
-        for(int i = 0; i < originalFileAttribute.length; i++){
-            if(!hasPipes.contains(i)){
-                sb.append(originalFileAttribute[i]);
-                sb.append(",");
-            }
-        }
-
-        // add the new attributes to the file
-        for(String s : pipedAttributes){
+        for(String s : newAttributes){
             sb.append(s);
             sb.append(",");
         }
 
-        sb.replace(sb.length() - 1, sb.length(),""); // remove the last comma
+        sb.delete(sb.length() - 1, sb.length());
 
         // write the new file attributes
-        PrintWriter writer = new PrintWriter(new File("Data/updatedPreprocessedFile"));
+        PrintWriter writer = new PrintWriter(new File("Data/updatedPreprocessedFile.csv"));
+
         writer.write(sb.toString());
 
-        newAttributes = new ArrayList<>();
-
-        for(String s : sb.toString().split(",")){
-            newAttributes.add(s);
-        }
-
-        Scanner scan = new Scanner(file);
-        scan.nextLine();
-        String[] line;
-        while(scan.hasNext()){
-            line = scan.nextLine().split(",");
-            for(int i = 0; i < line.length; i++) {
-                String value = 
+        for(ArrayList<String> row : newData){
+            sb = new StringBuilder();
+            for(int i = 0; i < row.size(); i++){
+                if(!hasPipes.contains(i)){
+                    sb.append(row.get(i));
+                    sb.append(",");
+                }
             }
+
+            for(int i = 0; i < row.size(); i++){
+                if (hasPipes.contains(i)) {
+                    for (String s : newAttributes) {
+                        String value = row.get(i);
+                        if (s.contains(value)) {
+                            sb.append(row.get(i));
+                            sb.append(",");
+                        } else {
+                            sb.append("null");
+                            sb.append(",");
+                        }
+                    }
+                }
+            }
+            writer.write(sb.toString());
+            writer.write("\n");
+            writer.flush();
         }
+
+        sb.delete(sb.length() - 1, sb.length());
+
         writer.close();
     }
 
